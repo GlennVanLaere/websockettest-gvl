a668f560201bea58dad501fe03beb6a7f2798107
GlennVanLaere <55496118+GlennVanLaere@users.noreply.github.com>
2021-04-01 23:44:19 +0200

    done

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -11,4 +11,10 @@ primus.on("data", (data) => {
     let emo = data.emotion;
     let width = document.querySelector(`.${emo} img`).style.width;
     console.log(width);
+
+    let px = width.substring(0, width.length-2);
+
+    px+=1.1;
+    
+    document.querySelector(`.${emo} img`).style.width = px + "px";
 })
